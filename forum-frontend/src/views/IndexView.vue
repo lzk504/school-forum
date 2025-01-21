68d33abd2b5ffb68fa9babe973bc263f4eae4389
lzk <729765221@qq.com>
2025-01-21 16:36:05 +0800

    修复侧边栏滑动框的问题

--- forum-frontend/src/views/IndexView.vue
+++ forum-frontend/src/views/IndexView.vue
@@ -7,7 +7,7 @@ import {
   Bell,
   ChatDotSquare, DataLine,
   Document, Files,
-  Location, Lock, Message, Monitor, Notebook,
+  Location, Lock, Message, Monitor, Collection,
   Notification, Operation,
   Position,
   School, Search,
@@ -87,130 +87,100 @@ function userLogout() {
         </div>
       </el-header>
       <el-container>
-        <el-aside width="220px">
-          <el-scrollbar style="min-height: calc(100vh - 55px)">
+        <el-aside width="230px">
+          <el-scrollbar style="height: calc(100vh - 55px)">
             <el-menu
                 router
                 :default-active="$route.path"
                 style="min-height: calc(100vh - 55px)">
               <el-sub-menu index="1">
                 <template #title>
-                  <el-icon>
-                    <Location/>
-                  </el-icon>
+                  <el-icon><Location/></el-icon>
                   <span><b>校园论坛</b></span>
                 </template>
                 <el-menu-item index="1-1">
                   <template #title>
-                    <el-icon>
-                      <ChatDotSquare/>
-                    </el-icon>
-                    <span>帖子广场</span>
+                    <el-icon><ChatDotSquare/></el-icon>
+                    帖子广场
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Bell/>
-                    </el-icon>
-                    <span>失物招领</span>
+                    <el-icon><Bell/></el-icon>
+                    失物招领
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Notification/>
-                    </el-icon>
-                    <span>校园活动</span>
+                    <el-icon><Notification/></el-icon>
+                    校园活动
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Umbrella/>
-                    </el-icon>
-                    <span>表白墙</span>
+                    <el-icon><Umbrella/></el-icon>
+                    表白墙
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <School/>
-                    </el-icon>
-                    <span>海文考研</span>
-                    <el-tag style="margin-left: 10px " size="small">合作机构</el-tag>
+                    <el-icon><School/></el-icon>
+                    海文考研
+                    <el-tag style="margin-left: 10px" size="small">合作机构</el-tag>
                   </template>
                 </el-menu-item>
               </el-sub-menu>
               <el-sub-menu index="2">
                 <template #title>
-                  <el-icon>
-                    <Position/>
-                  </el-icon>
+                  <el-icon><Position/></el-icon>
                   <span><b>探索与发现</b></span>
                 </template>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Document/>
-                    </el-icon>
-                    <span>成绩查询</span>
+                    <el-icon><Document/></el-icon>
+                    成绩查询
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Files/>
-                    </el-icon>
-                    <span>班级课程表</span>
+                    <el-icon><Files/></el-icon>
+                    班级课程表
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Monitor/>
-                    </el-icon>
-                    <span>教务通知</span>
+                    <el-icon><Monitor/></el-icon>
+                    教务通知
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Notebook/>
-                    </el-icon>
-                    <span>在线图书馆</span>
+                    <el-icon><Collection/></el-icon>
+                    在线图书馆
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <DataLine/>
-                    </el-icon>
-                    <span>预约教室</span>
+                    <el-icon><DataLine/></el-icon>
+                    预约教室
                   </template>
                 </el-menu-item>
               </el-sub-menu>
               <el-sub-menu index="3">
                 <template #title>
-                  <el-icon>
-                    <Operation/>
-                  </el-icon>
-                  <span><b>个人中心</b></span>
+                  <el-icon><Operation/></el-icon>
+                  <span><b>个人设置</b></span>
                 </template>
                 <el-menu-item index="/index/user-setting">
                   <template #title>
-                    <el-icon>
-                      <User/>
-                    </el-icon>
-                    <span>个人信息设置</span>
+                    <el-icon><User/></el-icon>
+                    个人信息设置
                   </template>
                 </el-menu-item>
                 <el-menu-item>
                   <template #title>
-                    <el-icon>
-                      <Lock/>
-                    </el-icon>
-                    <span>账户安全设置</span>
+                    <el-icon><Lock/></el-icon>
+                    账号安全设置
                   </template>
                 </el-menu-item>
               </el-sub-menu>
